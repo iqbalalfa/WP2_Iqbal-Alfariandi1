af66da10d7b34552ed58ce1beeb616c39a41474a
iqbalalfa <ialfariandi@gmail.com>
2022-10-15 01:55:52 +0700

    tugas pertemuan5

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['ModelUser'] = array('ModelBuku');
+$autoload['model'] = array('ModelUser', 'ModelBuku');
